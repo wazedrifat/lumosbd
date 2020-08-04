03cb717c948ccf6521eeb0eea9f0ecce2fe3d188
wazedrifat <34921453+wazedrifat@users.noreply.github.com>
2020-08-04 22:31:30 +0600

    leaderboard done but better needed

--- src/app/pages/dashboard/leaderboard/leaderboard.page.ts
+++ src/app/pages/dashboard/leaderboard/leaderboard.page.ts
@@ -13,7 +13,6 @@ import { Component, OnInit } from '@angular/core';
 
 export class LeaderboardPage implements OnInit {
 	
-
 	public dataList = [];
 
 	constructor(
@@ -21,48 +20,11 @@ export class LeaderboardPage implements OnInit {
 		private storage: Storage,
 		private http:HttpClient,
 	) {
-		console.log('leaderboard page');
-		
-		// this.dataList.push({
-		// 	points: '10.60',
-		// 	student_id: "11",
-		// 	exam_given: "1",
-		// 	name: "Farid Ahmed",
-		// 	rank: "1"
-		// });
-
-		// this.dataList.push({
-		// 	points: "9.7",
-		// 	student_id: "16",
-		// 	exam_given: "2",
-		// 	name: "rifat",
-		// 	rank: "2"
-		// });
-
-		// console.log('dataList : ' + this.dataList[0]);
 		this.authService.getLeaderBoardList().subscribe((data)=> {
-			console.log('data: ' + JSON.stringify(data));
+			this.dataList = data['leaderboard'];
 		}), (err => {
 			console.log('get leaderboard list error : ' + err);
 		});
-
-		// let headers = new HttpHeaders({
-		// 	'Authorization': AuthConstants.savedToken
-		// });
-		// let options = {headers:headers};
-
-		// this.http.get('http://api.lumosbd.com/v1/dashboard/get-leaderboard-list', options).subscribe((res)=> {
-		// 	console.log('http get : ' + JSON.stringify(res));
-		// });
-
-
-		// console.log('leaderboard');
-		// this.storage.get(AuthConstants.token).then((val)=> {
-		// 	if (val) {
-		// 		console.log('val found');
-		// 	}
-		// 	console.log('authorization : ' + val);
-		// });
 	}
 
 	ngOnInit() {
